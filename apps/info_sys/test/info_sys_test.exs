ca11fe1eb09576344af5ef9972def69c8c639492
fonglh <fonglh@gmail.com>
2016-11-09 22:15:24 +0100

    Tests in which backend properly reports results.
    Stub a backend.
    Add tests for having backend results and not having backend results.

--- apps/info_sys/test/info_sys_test.exs
+++ apps/info_sys/test/info_sys_test.exs
@@ -1,8 +1,30 @@
 defmodule InfoSysTest do
   use ExUnit.Case
-  doctest InfoSys
+  alias InfoSys.Result
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  # Create a stub TestBackend as we do not want to make network calls to the actual
+  # Wolfram Alpha service during tests.
+  # Get this stub to return data in the expected format.
+  defmodule TestBackend do
+    def start_link(query, ref, owner, limit) do
+      Task.start_link(__MODULE__, :fetch, [query, ref, owner, limit])
+    end
+
+    # query string used to specify which set of results to fetch
+    def fetch("result", ref, owner, _limit) do
+      send(owner, {:results, ref, [%Result{backend: "test", text: "result"}]})
+    end
+    def fetch("none", ref, owner, _limit) do
+      send(owner, {:results, ref, []})
+    end
+  end
+
+  test "compute/2 with backend results" do
+    assert [%Result{backend: "test", text: "result"}] =
+      InfoSys.compute("result", backends: [TestBackend])
+  end
+
+  test "compute/2 with no backend results" do
+    assert [] = InfoSys.compute("none", backends: [TestBackend])
   end
 end
