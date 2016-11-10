09e217861c9d21970dcd58f997f64e8abaa859e5
fonglh <fonglh@gmail.com>
2016-11-10 18:11:23 +0100

    Add test for timed out backend.
    Simulate timed out backend with fetch function.

--- apps/info_sys/test/info_sys_test.exs
+++ apps/info_sys/test/info_sys_test.exs
@@ -17,6 +17,32 @@ defmodule InfoSysTest do
     def fetch("none", ref, owner, _limit) do
       send(owner, {:results, ref, []})
     end
+
+    def fetch("timeout", _ref, owner, _limit) do
+      send(owner, {:backend, self()})
+      # simulates a request that takes too long
+      :timer.sleep(:infinity)
+    end
+  end
+
+  test "compute/2 with timeout returns no results and kills workers" do
+    # use the fetch parameter to simulate difficult behaviour
+    results = InfoSys.compute("timeout", backends: [TestBackend], timeout: 10)
+    # ensure 0 results after short 10ms timeout
+    assert results == []
+    # check that we received the backend_pid, then monitor it for the :DOWN message
+    # assert_receive waits 100ms by default, can be configured with a parameter
+    assert_receive {:backend, backend_pid}
+
+    ref = Process.monitor(backend_pid)
+    # check that code received :DOWN and killed the backend
+    assert_receive {:DOWN, ^ref, :process, _pid, _reason}
+    # refute_received makes sure there are no more :DOWN or :timedout messages
+    # in the inbox. compute was written to clean up after itself, so this checks
+    # the cleanup code.
+    # Note this is different from refute_receive, which will wait 100ms first.
+    refute_received {:DOWN, _, _, _, _}
+    refute_received :timedout
   end
 
   test "compute/2 with backend results" do
